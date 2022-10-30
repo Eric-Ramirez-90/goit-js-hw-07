133aee49bde67b66e6adbe74d1a07463d0ebf2cb
Eric Ramirez <ramirez1990rer@gmail.com>
2022-10-30 18:47:43 +0200

    Completed task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// console.log(basicLightbox);
+
+const refs = {
+  containerForMarkup: document.querySelector('.gallery'),
+};
+
+refs.containerForMarkup.addEventListener('click', onImageClick);
+
+const createElement = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+refs.containerForMarkup.insertAdjacentHTML('beforeend', createElement);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox
+    .create(
+      `<img
+          src="${event.target.dataset.source}"
+          alt="${event.target.alt}"
+        />`,
+      { closable: true }
+    )
+    .show();
+}
